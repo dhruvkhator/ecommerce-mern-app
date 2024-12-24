4c5350b483f7174772db1b6528ec0a58e7275a70
Dhruv Khator <dhruvkhator19@gmail.com>
2024-12-24 22:39:19 +0530

    test worflow trigger -9

--- server/auth-service/src/server.ts
+++ server/auth-service/src/server.ts
@@ -6,7 +6,7 @@ import authRoutes from './routes/authRoutes.js';
 import logger from './utils/logger.js';
 
 dotenv.config();
-//test worflow trigger -8
+//test worflow trigger -9
 const app: Application = express();
 const PORT = process.env.PORT || 5150;
 
