a6b68c130f9fd62bc076ad606f472a1cf359bcf7
Dhruv Khator <dhruvkhator19@gmail.com>
2024-12-25 17:54:41 +0530

    Updated the cookie setting for production

--- server/auth-service/src/controllers/authController.ts
+++ server/auth-service/src/controllers/authController.ts
@@ -83,6 +83,8 @@ export const signinUser = async (req: Request, res: Response) => {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: 'none',
+            domain:'.onrender.com',
+            path:'/',
             maxAge: 24 * 60 * 60 * 1000,
         });
 
